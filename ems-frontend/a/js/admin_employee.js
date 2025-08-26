6f496eb8665414467af34816a7afdfbf3e5b0b59
skulk0156 <skulk156.pc@gmail.com>
2025-08-26 18:23:14 +0530

    commit

--- ems-frontend/a/js/admin_employee.js
+++ ems-frontend/a/js/admin_employee.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'http://localhost:3001/api';
+    const API_BASE_URL = '/api';
 
     const addEmployeeBtn = document.getElementById('addEmployeeBtn');
     const addEmployeeModal = document.getElementById('addEmployeeModal');
