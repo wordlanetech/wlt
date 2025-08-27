a8f5ef3564ceb93c36c8b05ecb7c1d1f668a1fc8
skulk0156 <skulk156.pc@gmail.com>
2025-08-27 17:10:29 +0530

    project

--- ems-backend/routes/projectRoutes.js
+++ ems-backend/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('../controllers/profileController');
+const profileController = require('../controllers/projectController');
 
 // GET profile by employee ID
 router.get('/:userId', profileController.getProfileByUserId);
