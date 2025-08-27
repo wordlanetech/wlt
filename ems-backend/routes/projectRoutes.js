3dec256fbf3e7bb9deec961634f56e177312eaf1
skulk0156 <skulk156.pc@gmail.com>
2025-08-27 17:32:46 +0530

    c

--- ems-backend/routes/projectRoutes.js
+++ ems-backend/routes/projectRoutes.js
@@ -1,9 +1,13 @@
+// routes/projectRoutes.js
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
+const authMiddleware = require('../middleware/auth.middleware');
 
-// Correct:
-router.get('/projects', projectController.getAllProjects);
-router.post('/projects', projectController.createProject);
+// GET /api/projects - get all projects
+router.get('/', authMiddleware, projectController.getProjects);
+
+// POST /api/projects - create a project
+router.post('/', authMiddleware, projectController.createProject);
 
 module.exports = router;
