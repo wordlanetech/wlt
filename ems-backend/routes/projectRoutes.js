35ec33e0e7147354f50a9b95daa69c00b972b9cd
skulk0156 <skulk156.pc@gmail.com>
2025-08-27 17:15:53 +0530

    project

--- ems-backend/routes/projectRoutes.js
+++ ems-backend/routes/projectRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('../controllers/projectController');
+const projectController = require('../controllers/projectController');
 
-// GET project by project ID
-router.get('/:projectId', projectController.getProjectById);
+// Correct:
+router.get('/projects', projectController.getAllProjects);
+router.post('/projects', projectController.createProject);
 
 module.exports = router;
