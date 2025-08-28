a54a7161ce1408d5c1b364196873cf4300251c87
skulk0156 <skulk156.pc@gmail.com>
2025-08-28 15:04:10 +0530

    hr

--- ems-backend/controllers/auth.controller.js
+++ ems-backend/controllers/auth.controller.js
@@ -37,7 +37,7 @@ exports.login = async (req, res) => {
         // ✅ Absolute URL paths for frontend
         const rolePages = {
             1: '/ems-frontend/a/admin_dashboard.html',
-            2: '/h/hr_dashboard.html',
+            2: 'h/hr_dashboard.html',
             3: '/ems-frontend/m/manager_dashboard.html',
             4: '/ems-frontend/e/dashboard.html',
         };
